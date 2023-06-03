a47ba0da52a75f5eef19d009fc8413431d98b0c4
Jay Medina <jay.medina.celis@gmail.com>
2023-06-02 21:23:28 -0400

    Adds tag buttons and styles form

--- blog-form/src/components/BookmarkingApp.jsx
+++ blog-form/src/components/BookmarkingApp.jsx
@@ -5,7 +5,19 @@ export default function BookmarkingApp() {
   const [url, setUrl] = useState("");
   const [tag, setTag] = useState("");
   const [bookmarkList, setBookmarkList] = useState([]);
+
   const [filteredList, setFilteredList] = useState([]);
+
+  const [filter, setFilter] = useState("");
+
+  const tagList = [...new Set(bookmarkList.map((bookmark) => bookmark.tag))];
+  const bookmarksHTML = bookmarkList
+    .filter((bookmark) => (filter ? bookmark.tag === filter : bookmark))
+    .map((bookmark, index) => (
+      <li key={index}>
+        <a href={bookmark.url}>{bookmark.title}</a>
+      </li>
+    ));
 
   function addToBookmarks(event) {
     event.preventDefault();
@@ -18,50 +30,60 @@ export default function BookmarkingApp() {
     setTag("");
   }
 
-  function filter(tag) {
-    setFilteredList(bookmarkList.filter((bookmark) => bookmark.tag === tag));
-  }
+  //   function filter(tag) {
+  //     setFilteredList(bookmarkList.filter((bookmark) => bookmark.tag === tag));
+  //   }
 
   return (
     <>
       <h1>Bookmarking App</h1>
-      <form onSubmit={addToBookmarks}>
-        <input
-          type="text"
-          value={title}
-          placeholder="Title"
-          onChange={(e) => setTitle(e.target.value)}
-        />
-        <input
-          type="url"
-          value={url}
-          placeholder="URL"
-          onChange={(e) => setUrl(e.target.value)}
-        />
-        <input
-          type="text"
-          value={tag}
-          placeholder="Tag"
-          onChange={(e) => setTag(e.target.value)}
-        />
-        <button type="submit">Submit</button>
-      </form>
-      <ul>
-        {bookmarkList.map((bookmark, index) => (
-          <li key={index}>
-            <a href={bookmark.url}>{bookmark.title}</a>
-            <button onClick={() => filter(bookmark.tag)}>{bookmark.tag}</button>
-          </li>
-        ))}
-      </ul>
-      <h2>Filtered Tags</h2>
-      <ul>
-        {filteredList.map((filteredItem, index) => (
-          <li key={index}>
-            <a href={filteredItem.url}>{filteredItem.title}</a>
-          </li>
-        ))}
-      </ul>
+      <div
+        style={{
+          backgroundColor: "skyblue",
+          padding: "20px",
+          width: "300px",
+          margin: "0 auto",
+        }}
+      >
+        <form
+          onSubmit={addToBookmarks}
+          style={{
+            backgroundColor: "skyblue",
+            display: "flex",
+            flexDirection: "column",
+            width: "90%",
+          }}
+        >
+          <input
+            type="text"
+            value={title}
+            placeholder="Title"
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <input
+            type="url"
+            value={url}
+            placeholder="URL"
+            onChange={(e) => setUrl(e.target.value)}
+          />
+          <input
+            type="text"
+            value={tag}
+            placeholder="Tag"
+            onChange={(e) => setTag(e.target.value)}
+          />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+
+      <button onClick={() => setFilter("")}>All</button>
+      {tagList.map((tag, index) => (
+        <button key={index} onClick={() => setFilter(tag)}>
+          {tag}
+        </button>
+      ))}
+
+      <ul>{bookmarksHTML}</ul>
     </>
   );
 }
